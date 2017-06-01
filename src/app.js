8aef14cbfb83bab579c4eccbf7743c71bf8711f8
Javier <javierho93@hotmail.com>
2017-06-02 00:41:14 +0100

    Controller imported

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import angular from 'angular';
+//Styles
 import '../css/style.scss';
+//Controllers
+import HomeController from './home/home.controller';
 
-const app = angular.module('filmboard', []);
+const app = angular.module('filmboard', [])
+  .controller('homeController', HomeController);
 
 export default app;
